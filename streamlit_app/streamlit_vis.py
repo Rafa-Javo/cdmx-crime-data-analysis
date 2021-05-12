3b7b8d097da180fb6ce75d38c377ac3d9f92373f
Javier Navarro <javojavojavojavo@gmail.com>
2021-05-12 15:07:59 -0500

    several..

--- streamlit_app/streamlit_vis.py
+++ streamlit_app/streamlit_vis.py
@@ -19,60 +19,56 @@ month_to_num = {key: i+1 for i,key in enumerate(month_days)}
 num_to_month = {(i+1): key for i,key in enumerate(month_days)}
 
 #! leemos los datos
-DATA_URL = ("delitos.xlsx")
+DATA_URL = ("purged_carpetas_de_inv_pgj_cdmx.csv")
 # DATA_URL = ("data/Delitos Alto Impacto municipio Morelia 2018-2019-2020.xlsx")
 
 
-st.title("Delitos Alto Impacto municipio Morelia 2018-2019-2020")
+st.title("Carpetas de Investigación CDMX - datos de la pgj")
 
 
 @st.cache(persist=False)
 def load_data():
-    data = pd.read_excel(DATA_URL,na_values='NaN')
-    data.FechaComision = pd.to_datetime(data.FechaComision)
-    data.rename(columns={'Longitud':'lon','Latitud':'lat','Delito':'delito'}, inplace=True)
+    data = pd.read_csv(DATA_URL,na_values='NaN')
+    data.fecha_hechos = pd.to_datetime(data.fecha_hechos)
+    data.rename(columns={'longitud':'lon','latitud':'lat'}, inplace=True)
     return data
 
 data = load_data()
 
 
-crimenes_por_tipo = data['delito'].value_counts()
-#crimenes_por_delegacion = crimenes_por_delegacion.loc[delegaciones]
-#st.write(crimenes_por_delegacion)
-crimenes_por_tipo = pd.DataFrame({'Tipo':crimenes_por_tipo.index, 'Crímenes':crimenes_por_tipo.values})
+
 
 # ------------------------------------------------------------------------------------------------------ #
-
-# HISTOGRAMA: (DELITOS x CATEGORÍA)
-st.sidebar.markdown('## Crímenes por tipo')
-if  not st.sidebar.checkbox("Ocultar", False,key='2sd'):
-    st.markdown('## Crímenes por tipo')
-    fig = px.bar(crimenes_por_tipo, x='Tipo', y='Crímenes', height=500) #, color='Crímenes'
-    st.plotly_chart(fig)
-
-# SERIE DE TIEMPO: (DELITOS x UNIDAD DE TIEMPO)
-#   Poder agregar varios plots en los mismos ejes para comparar 2 o más años,meses, semanas, etc.
-#   - Por ventana de tiempo específica - poder comparar con otra ventana de tiempo del *mismo tamaño
-#   - Por año, por mes, por semana, por dia, por hora - '' del mismo tamaño
-
-# ------------------------------------------------------------------------------------------------------ #
-
-# MAPA (3 mapas)
-st.sidebar.markdown('## Mapas')
+# FILTRANDO DATOS
+#data = data[data['fecha_hechos'].notna()]
+data = data.dropna()
+st.sidebar.markdown('# Filtros')
+st.sidebar.markdown('## Delitos')
 choice = st.sidebar.multiselect('Escoge delito(s)', (sorted(list(data.delito.unique()))), key='54')
 
 st.sidebar.markdown("### Horario")
 if not st.sidebar.checkbox("Todo el día", True,key='2'):
-    hour = st.sidebar.slider('Hora del día', 0,23)
-    hour = [hour]
+    #start = st.sidebar.number_input('Hora inicio', min_value=0, max_value=23, value=5, step=1)
+    #end = st.sidebar.number_input('Hora fin', min_value=0, max_value=23, value=8, step=1)
+    #hour = st.sidebar.slider('Hora del día', 0,23)
+    #if start < end:
+    #    hour = list(range(start,end))
+    #    st.sidebar.markdown('Horas: ['+str(start)+','+str(end)+')')
+    #else:
+    #    hour = list(range(start,24)) + list(range(0,end))
+    #    st.sidebar.markdown('Horas: ['+str(start)+','+'24'+')'+' U '+'[0,'+str(end)+')')
+    
+    #hour = [hour]
+    hour = st.sidebar.multiselect('Horas:', list(range(24)), default=[5,6,7,8])
     
 else:
-    hour = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+    hour = list(range(24))
 
 meses = st.sidebar.multiselect('Meses', 
 ('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'), default=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'])
 
-anios = st.sidebar.multiselect('Años', ('2018','2019','2020'), default = ['2018','2019','2020'])
+list(data.fecha_hechos.dt.year.unique())
+anios = st.sidebar.multiselect('Años', (list(range(2016,2020))), default = list(range(2016,2020)))
 
 subdata = data
 
@@ -81,82 +77,98 @@ if len(choice) > 0:
     subdata = subdata[subdata.delito.isin(choice)]
 
 # Filtrando por rango de horas
-subdata = subdata[subdata['FechaComision'].dt.hour.isin(hour)]
+subdata = subdata[subdata['fecha_hechos'].dt.hour.isin(hour)]
 
 # Filtrando por meses
 nums = []
 for mes in meses:
     nums.append(month_to_num[mes])
-subdata = subdata[subdata['FechaComision'].dt.month.isin(nums)]
+subdata = subdata[subdata['fecha_hechos'].dt.month.isin(nums)]
 
 # Filtrando por anio
-subdata = subdata[subdata['FechaComision'].dt.year.isin(anios)]
+subdata = subdata[subdata['fecha_hechos'].dt.year.isin(anios)]
 
 # Especificando la cantidad de delitos en la visualización
-st.markdown('## {} delitos cometidos con los parámetros especificados.'.format(subdata.shape[0]))
+st.markdown('## {} delitos registrados.'.format(subdata.shape[0]))
+
+# ------------------------------------------------------------------------------------------------------ #
 
 
+# HISTOGRAMA: (DELITOS x CATEGORÍA)
+crimenes_por_tipo = subdata['delito'].value_counts()
+crimenes_por_tipo = pd.DataFrame({'Tipo':crimenes_por_tipo.index, 'Crímenes':crimenes_por_tipo.values})
+st.markdown('### Crímenes por tipo')
+fig = px.bar(crimenes_por_tipo, x='Tipo', y='Crímenes', height=500) #, color='Crímenes'
+st.plotly_chart(fig)
 
+# SERIE DE TIEMPO: (DELITOS x UNIDAD DE TIEMPO)
+#   Poder agregar varios plots en los mismos ejes para comparar 2 o más años,meses, semanas, etc.
+#   - Por ventana de tiempo específica - poder comparar con otra ventana de tiempo del *mismo tamaño
+#   - Por año, por mes, por semana, por dia, por hora - '' del mismo tamaño
+
+# ------------------------------------------------------------------------------------------------------ #
+# MAPA (3 mapas)
 # Mapa con pydeck
-coor = subdata[['lat','lon']]
-st.pydeck_chart(
-    pdk.Deck(
-        map_style='mapbox://styles/mapbox/streets-v11',
-        initial_view_state = pdk.ViewState(
-            latitude = 19.70202427849626,
-            longitude=-101.19298171815072,
-            zoom=12,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data = coor,
-                get_position='[lon,lat]',
-                get_fill_color='[200,20,0,400]',
-                get_radius= 25,
-            ),
-        ]
-    )
-)
+#coor = subdata[['lat','lon']]
+#st.pydeck_chart(
+#    pdk.Deck(
+#        map_style='mapbox://styles/mapbox/streets-v11',
+#        initial_view_state = pdk.ViewState(
+#            latitude = 19.425821,
+#            longitude= -99.1897989,
+#            zoom=10,
+#            pitch=50,
+#        ),
+#        layers=[
+#           pdk.Layer(
+#                'ScatterplotLayer',
+#                data = coor,
+#                get_position='[lon,lat]',
+#                get_fill_color='[200,20,0,400]',
+#                get_radius= 25,
+#            ),
+#        ]
+#    )
+#)
 
 #st.write(subdata)    # DA ERROR ESTA LÍNEA NO SÉ PORQUÉ
 
 
 #Mapas con folium (coloreado y hotspots)
 #print(subdata)
-if st.checkbox("Mapa coloreado por tipo de crímen", False,key='asd2'):
-    morelia = (19.70078, -101.18443)
-    m = folium.Map(location = morelia, tiles = 'openstreetmap', zoom_start = 13) 
-    for data in zip(subdata.lat, subdata.lon, subdata.FechaComision, subdata.delito):
-        # print(data[0], data[1])
-        p = data[2]
-        c = 'black'
-        if data[3] == 'Feminicidio':
-            c = 'purple'
-        elif data[3] == 'Homicidio doloso':
-            c = 'red'
-        elif data[3] == 'Robo a casa habitación':
-            c = 'blue'
-        elif data[3] == 'Robo a comercios':
-            c = 'green'
-        elif data[3] == 'Robo a transeúnte':
-            c = 'yellow'
-        elif data[3] == 'Robo de vehículo':
-            c = 'grey'
-        elif data[3] == 'Extorsión':
-            c = 'orange'
-        folium.CircleMarker(location=[data[0], data[1]], radius=2,popup = p, color = c).add_to(m)
-    folium_static(m)
+#if st.checkbox("Mapa coloreado por tipo de crímen", False,key='asd2'):
+#    morelia = (19.70078, -101.18443)
+#    m = folium.Map(location = morelia, tiles = 'openstreetmap', zoom_start = 13) 
+#    for data in zip(subdata.lat, subdata.lon, subdata.fecha_hechos, subdata.delito):
+#        # print(data[0], data[1])
+#        p = data[2]
+#        c = 'black'
+#        if data[3] == 'Feminicidio':
+#            c = 'purple'
+#        elif data[3] == 'Homicidio doloso':
+#            c = 'red'
+#        elif data[3] == 'Robo a casa habitación':
+#            c = 'blue'
+#        elif data[3] == 'Robo a comercios':
+#            c = 'green'
+#        elif data[3] == 'Robo a transeúnte':
+#            c = 'yellow'
+#        elif data[3] == 'Robo de vehículo':
+#            c = 'grey'
+#        elif data[3] == 'Extorsión':
+#            c = 'orange'
+#        folium.CircleMarker(location=[data[0], data[1]], radius=2,popup = p, color = c).add_to(m)
+#    folium_static(m)
 
 # Mapa 2 (heatmap)
-if st.checkbox("Mapa de calor", False,key='aasdfsd2'):
-    heat=subdata[['lat','lon']]
-    heat.lat.fillna(0,inplace=True)
-    heat.lon.fillna(0,inplace=True)
-    m6=folium.Map(location=[19.70078, -101.18443],tiles='Stamen Toner',zoom_start=13)
-    HeatMap(data=heat,radius=9.5).add_to(m6)
-    folium_static(m6)
+#if st.checkbox("Mapa de calor", False,key='aasdfsd2'):
+st.markdown('### Mapa de calor')
+heat=subdata[['lat','lon']]
+heat.lat.fillna(0,inplace=True)
+heat.lon.fillna(0,inplace=True)
+m6=folium.Map(location=[19.425821, -99.1897989],tiles='Stamen Toner',zoom_start=10)
+HeatMap(data=heat,radius=9.5).add_to(m6)
+folium_static(m6)
 
 
 
@@ -164,7 +176,7 @@ if st.checkbox("Mapa de calor", False,key='aasdfsd2'):
 
 
 # COMPARATIVA ANUAL
-st.markdown('## Comparativa anual con los parámetros especificados')
+st.markdown('### Comparativa anual con los parámetros especificados')
 intmeses = [month_to_num[i] for i in meses]
 for m in range(1,13):
     if m not in intmeses:
@@ -175,10 +187,10 @@ gra['meses'] = [num_to_month[i][:3] for i in range(1,13)]
 gra.set_index('meses',inplace=True)
 sns.set_style("darkgrid")
 for año in anios:
-    time_series = subdata.drop(columns=['lat','lon','Fiscalia','Municipio','Colonia','CalleNumero','Lugar','Arma','Violencia','FechaInicio'])
+    time_series = subdata[['fecha_hechos']]#subdata.drop(columns=['lat','lon','Fiscalia','Municipio','Colonia','CalleNumero','Lugar','Arma','Violencia','FechaInicio'])
     #st.write(time_series)          # DA ERROR ESTA LÍNEA NO SÉ PORQUÉ
-    time_series = time_series[time_series.FechaComision.dt.year == int(año)]
-    time_series['meses'] = time_series.FechaComision.dt.month
+    time_series = time_series[time_series.fecha_hechos.dt.year == int(año)]
+    time_series['meses'] = time_series.fecha_hechos.dt.month
     cri_mes = []
     mes_es = []
     for i in range(1,13):
@@ -196,17 +208,17 @@ st.pyplot()
 
 
 #Crimenes por violencia
-st.markdown('## Comparativa de crímenes con violencia')
+#st.markdown('## Comparativa de crímenes con violencia')
 
 # print(subdata)
-st.set_option('deprecation.showPyplotGlobalUse', False)
-if len(choice) >= 4 or len(choice) == 0:
-    fig, axs = plt.subplots(figsize=(15,7.5))
-ax = sns.countplot(x = 'delito', hue = 'Violencia', data=subdata)
-ax.set(ylabel = "Crímenes")
-ax.set(xlabel = "Dellito")
+#st.set_option('deprecation.showPyplotGlobalUse', False)
+#if len(choice) >= 4 or len(choice) == 0:
+#    fig, axs = plt.subplots(figsize=(15,7.5))
+#ax = sns.countplot(x = 'delito', hue = 'Violencia', data=subdata)
+#ax.set(ylabel = "Crímenes")
+#ax.set(xlabel = "Dellito")
 
-st.pyplot()
+#st.pyplot()
 
 # SERIE DE TIEMPO: (DELITOS x UNIDAD DE TIEMPO)
 #   ** Hay que discretizar los valores por un rango de tiempo
@@ -226,53 +238,62 @@ st.pyplot()
 
 # DISTRIBUCIÓN DE CRÍMENES POR HORA
 #st.sidebar.markdown("### Usando parámetros especificados:")
-st.sidebar.markdown("### Distribución por hora")
-if st.sidebar.checkbox("Mostrar", False, key='dcph'):
-	st.markdown("### Distribución por hora")
-	horas = [i for i in range(0,24)]
-	distribucion = []
-	for i,h in enumerate(horas):
-		subd = subdata[subdata['FechaComision'].dt.hour == h]
-		distribucion.append( subd.shape[0])
-	df = pd.DataFrame(
-						{'hora': horas,
-						'delitos': distribucion
-						})
-	fig = px.bar(df, x='hora', y='delitos', height=500) #, color='Crímenes'
-	st.plotly_chart(fig)
+#st.sidebar.markdown("### Distribución por hora")
+#if st.sidebar.checkbox("Mostrar", False, key='dcph'):
+st.markdown("### Distribución por hora")
+horas = [i for i in range(0,24)]
+distribucion = []
+for i,h in enumerate(horas):
+    subd = subdata[subdata['fecha_hechos'].dt.hour == h]
+    distribucion.append( subd.shape[0])
+df = pd.DataFrame(
+                    {'hora': horas,
+                    'delitos': distribucion
+                    })
+fig = px.bar(df, x='hora', y='delitos', height=500) #, color='Crímenes'
+st.plotly_chart(fig)
 
 
 # DISTRIBUCIÓN DE CRÍMENES POR DIA DE LA SEMANA
-st.sidebar.markdown("### Distribución por día de la semana")
-if st.sidebar.checkbox("Mostrar", False, key='dcpds'):
-	st.markdown("### Distribución por día de la semana")
-	dia_semana = [i for i in range(0,7)]
-	dia_str = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo']
-	distribucion = []
-	for i,d in enumerate(dia_semana):
-		subd = subdata[subdata['FechaComision'].dt.dayofweek == d]
-		distribucion.append( subd.shape[0])
-	df = pd.DataFrame(
-						{'dia': dia_str,
-						'delitos': distribucion
-						})
-	fig = px.bar(df, x='dia', y='delitos', height=500) #, color='Crímenes'
-	st.plotly_chart(fig)
+#st.sidebar.markdown("### Distribución por día de la semana")
+#if st.sidebar.checkbox("Mostrar", False, key='dcpds'):
+st.markdown("### Distribución por día de la semana")
+dia_semana = [i for i in range(0,7)]
+dia_str = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo']
+distribucion = []
+for i,d in enumerate(dia_semana):
+    subd = subdata[subdata['fecha_hechos'].dt.dayofweek == d]
+    distribucion.append( subd.shape[0])
+df = pd.DataFrame(
+                    {'dia': dia_str,
+                    'delitos': distribucion
+                    })
+fig = px.bar(df, x='dia', y='delitos', height=500) #, color='Crímenes'
+st.plotly_chart(fig)
 
 
 # DISTRIBUCIÓN DE CRÍMENES POR MES
-st.sidebar.markdown("### Distribución por mes")
-if st.sidebar.checkbox("Mostrar", False, key='dpm'):
-	st.markdown("### Distribución por mes")
-	mes = [i for i in range(1,13)]
-	mes_str = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
-	distribucion = []
-	for i,m in enumerate(mes):
-		subd = subdata[subdata['FechaComision'].dt.month == m]
-		distribucion.append( subd.shape[0])
-	df = pd.DataFrame(
-						{'mes': mes_str,
-						'delitos': distribucion
-						})
-	fig = px.bar(df, x='mes', y='delitos', height=500) #, color='Crímenes'
-	st.plotly_chart(fig)
+#st.sidebar.markdown("### Distribución por mes")
+#if st.sidebar.checkbox("Mostrar", False, key='dpm'):
+st.markdown("### Distribución por mes")
+mes = [i for i in range(1,13)]
+mes_str = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
+distribucion = []
+for i,m in enumerate(mes):
+    subd = subdata[subdata['fecha_hechos'].dt.month == m]
+    distribucion.append( subd.shape[0])
+df = pd.DataFrame(
+                    {'mes': mes_str,
+                    'delitos': distribucion
+                    })
+fig = px.bar(df, x='mes', y='delitos', height=500) #, color='Crímenes'
+st.plotly_chart(fig)
+
+
+# ------------------------------------------------------------------------------------------------------ #
+# INFORMACIÓN
+
+st.markdown("### Información")
+st.markdown("- Los datos son de libre acceso y fueron obtenidos el 3 de diciembre de 2020 antes de las 20:44hrs en [este link](https://datos.cdmx.gob.mx/explore/dataset/carpetas-de-investigacion-pgj-cdmx/export/?dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQ09VTlQiLCJ5QXhpcyI6ImxvbiIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6InJhbmdlLUFjY2VudCJ9XSwieEF4aXMiOiJhb19oZWNob3MiLCJtYXhwb2ludHMiOjUwLCJ0aW1lc2NhbGUiOiIiLCJzb3J0IjoiIiwic2VyaWVzQnJlYWtkb3duIjoiZGVsaXRvIiwic2VyaWVzQnJlYWtkb3duVGltZXNjYWxlIjoiIiwic3RhY2tlZCI6Im5vcm1hbCIsImNvbmZpZyI6eyJkYXRhc2V0IjoiY2FycGV0YXMtZGUtaW52ZXN0aWdhY2lvbi1wZ2otY2RteCIsIm9wdGlvbnMiOnsicmVmaW5lLmRlbGl0byI6IlZJT0xBQ0lPTiJ9fX1dLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlLCJ0aW1lc2NhbGUiOiIifQ%3D%3D).    \n- El código de este proyecto está aquí: https://github.com/Rafa-Javo/cdmx-crime-data-analysis.     \n- Es importante señalar que los crímenes denunciados/registrados representan solo una porción de los crímenes totales cometidos. En la [ENVIPE 2020](https://www.inegi.org.mx/contenidos/programas/envipe/2020/doc/envipe2020_mex.pdf) se estima que a nivel nacional, en 2019, se denunciaron 11% de los delitos. Y en 69.1% de los casos se inició una Carpeta de Investigación.    \n- La fecha utilizada en las visualizaciones es la fecha de los hechos, no la fecha del inicio de la carpeta de investigación. \n- Sólo hay datos para la mitad de 2019, por lo que para ver la distribución mensual se recomienda omitir ese año y solo incluir de 2016-2018.")
+'''
+'''
